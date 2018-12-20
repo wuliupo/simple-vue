53bcc32dadecffe411f61a9f306df4881b64d18d
liupo.wu <liupo.wu@wz-inc.com>
2018-12-20 19:39:24 +0800

    html  title change

--- js/app.js
+++ js/app.js
@@ -4,10 +4,17 @@
     window.SIMPLE_VUE = window.SIMPLE_VUE || {};
     window.SIMPLE_VUE.PAGES = window.SIMPLE_VUE.PAGES || {};
     Vue.prototype.$eventbus = new Vue(); // 用于事件通知
+    var router = new VueRouter({
+        mode: 'hash',
+        routes: window.SIMPLE_VUE.routes
+    });
+    router.beforeEach(function(to, form, next) {
+        if (to.meta && to.meta.nav) {
+            document.title = to.meta.nav + ' - Simple Vue demo';
+        }
+        next();
+    });
     window.SIMPLE_VUE.instance = new Vue({
-        router: new VueRouter({
-            mode: 'hash',
-            routes: window.SIMPLE_VUE.routes
-        })
+        router: router
     }).$mount('#app');
 }();
